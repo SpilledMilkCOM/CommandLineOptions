c83c92e5168f5cd44d834dcec60f435f9843becf
Parker Smart <psmart@spilledmilk.com>
2026-01-01 14:45:57 -0700

    Fix the "lifetime"

--- src/ConsoleAppHost/ConsoleService.cs
+++ src/ConsoleAppHost/ConsoleService.cs
@@ -11,11 +11,13 @@ namespace ConsoleAppHost
     {
         private readonly ILogger<ConsoleService> _logger;
         private readonly IOptions<ApplicationSettings> _options;
+        private readonly IHostApplicationLifetime _lifetime;
 
-        public ConsoleService(ILogger<ConsoleService> logger, IOptions<ApplicationSettings> options)
+        public ConsoleService(ILogger<ConsoleService> logger, IOptions<ApplicationSettings> options, IHostApplicationLifetime lifetime)
         {
             _logger = logger;
             _options = options;
+            _lifetime = lifetime;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -24,7 +26,7 @@ namespace ConsoleAppHost
                 _options.Value.Message, _options.Value.Verbose, _options.Value.LoopCount);
 
             int count = 0;
-            
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("{Time}: {Message}", DateTimeOffset.Now, _options.Value.Message);
@@ -34,6 +36,7 @@ namespace ConsoleAppHost
                 if (_options.Value.LoopCount > 0 && count >= _options.Value.LoopCount)
                 {
                     _logger.LogInformation("Reached loop count of {LoopCount}, stopping", _options.Value.LoopCount);
+                    _lifetime.StopApplication();
                     break;
                 }
 
